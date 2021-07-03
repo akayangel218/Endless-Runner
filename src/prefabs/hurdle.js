a6c35a0e0f4668ab59d28f85c1ca1c8eca8ea2f8
Kidus Michael <kimichae@ucsc.edu>
2021-07-03 12:49:13 -0700

    Giving a head start

--- src/prefabs/hurdle.js
+++ src/prefabs/hurdle.js
@@ -1,1 +1,8 @@
 // Hurdle prefab
+class Hurdle extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        //this.moveSpeed = game.settings.hurdleSpeed;
+    }
+}
