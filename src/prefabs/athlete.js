20d46a0634fad3920c5296a6e52ef53e4f315551
Mateen <maaminia@gmail.com>
2021-07-05 12:20:31 -0700

    Fixed 'moveSpeed' bug in athlete.js

--- src/prefabs/athlete.js
+++ src/prefabs/athlete.js
@@ -3,15 +3,14 @@ class Athlete extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
-        this.moveSpeed = 3;           // pixels per frame
+        this.moveSpeed = 3; // pixels per frame
     }
 
     update() {
         // left/right movement
         if (keyLEFT.isDown) {
-            this.x -= moveSpeed;
-        } 
-        else if (keyRIGHT.isDown) {
+            this.x -= this.moveSpeed;
+        } else if (keyRIGHT.isDown) {
             this.x += this.moveSpeed;
         }
 
