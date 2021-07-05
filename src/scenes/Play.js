158df49bdca7486cb00e1758ce20162c1a24de89
Kidus Michael <kimichae@ucsc.edu>
2021-07-05 12:04:10 -0700

    So right movement is working rn.

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -25,15 +25,15 @@ class Play extends Phaser.Scene {
 
         // green UI background
         this.add.rectangle(0, 0, game.config.width, borderUISize * 2, 0x0000FF).setOrigin(0, 0);
-
-        // add player
-        this.p1athlete = new Athlete(this, game.config.width/2, game.config.height - borderUISize - 10, 'athlete').setOrigin(0.5, 0);
         
         // add hurdles
         this.hurdle01 = new Hurdle(this, game.config.height - borderUISize * 4, borderUISize * 4, 'hurdle', 0, 30).setOrigin(0, 0);
         this.hurdle01.setScale(.15);
         //this.hurdle02 = new Hurdle(this, game.config.height - borderUISize * 3, borderUISize * 5 + borderPadding * 2, 'hurdle', 0, 20).setOrigin(0, 0);
         //this.hurdle02.setScale(.3);
+
+        // add player
+        this.p1athlete = new Athlete(this, game.config.width/2, game.config.height - borderUISize - 30, 'athlete').setOrigin(0.5, 0);
 
         // define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
