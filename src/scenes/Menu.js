e11d296217a1a1f7e3a8c66fedc1f57e3efa72eb
Mateen <maaminia@gmail.com>
2021-07-07 14:51:29 -0700

    Added instructions to menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -19,9 +19,27 @@ class Menu extends Phaser.Scene {
             fixedWidth: 0
         }
 
+        // menu text configuration
+        let extraConfig = {
+            fontFamily: 'Courier',
+            fontSize: '17px',
+            //backgroundColor: 'white',
+            color: 'white',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+                right: 10,
+                left: 10,
+            },
+            fixedWidth: 0
+        }
+
         // show menu text
-        this.add.text(game.config.width / 2, game.config.height / 2 - borderUISize - borderPadding, 'Infinite Meter Dash', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width / 2, game.config.height / 2, 'Press S to start', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 4 - borderUISize - borderPadding, 'Infinite Meter Dash', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 3, 'Press S to start', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 2, 'Press the LEFT and RIGHT arrow keys to move,', extraConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 1.8, 'Press SPACE to jump over the obstacles.', extraConfig).setOrigin(0.5);
         //menuConfig.backgroundColor = '#00FF00';
         //menuConfig.color = '#000';
 
